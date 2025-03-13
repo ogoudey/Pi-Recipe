c81a2d778a0858e471e3f418be7e1869b706473a
Olin <olin.goog@gmail.com>
2025-03-12 21:46:22 -0400

    ready for deployment

--- basics.py
+++ basics.py
@@ -4,27 +4,53 @@ from time import sleep
 import math
 
 # run with: GPIOZERO_PIN_FACTORY=lgpio python3 basics.py
+class MyMotor:
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+        self.motor = Motor(num1, num2)
+        pass
+    def forward(self):
+        print("motor with pin ", str(self.num1), str(self.num2), "forward")
+    def backward(self):
+        print("motor with pin ", str(self.num1), str(self.num2), "backward")
+    def stop(self):
+        print("motor with pin ", str(self.num1), str(self.num2), "stopping")
+
+motor_bl = MyMotor(17, 27)
+motor_fl = MyMotor(22, 23)
+motor_br = MyMotor(24, 25)
+motor_fr = MyMotor(5, 6)
 
 
-motor_bl = Motor(17, 27)
-motor_fl = Motor(22, 23)
-motor_br = Motor(24, 25)
-motor_fr = Motor(5, 6)
 
+def test_motor(char):
+    if char == "q":
+        motor_bl.forward()
+    elif char == "a":
+        motor_bl.stop()
+    elif char == "z":
+        motor_bl.backward()
+    elif char == "w":
+        motor_fl.forward()
+    elif char == "s":
+        motor_fl.stop()
+    elif char == "x":
+        motor_fl.backward()
+    if char == "e":
+        motor_br.forward()
+    elif char == "d":
+        motor_br.stop()
+    elif char == "c":
+        motor_br.backward() 
+    elif char == "r":
+        motor_fr.forward()
+    elif char == "f":
+        motor_fr.stop()
+    elif char == "v":
+        motor_fr.backward()  
 
-def test_motors():
-    motor_bl.forward()
-    sleep(1)
-    motor_bl.stop()
-    motor_fl.forward()
-    sleep(1)
-    motor_fl.stop()
-    motor_br.forward()
-    sleep(1)
-    motor_br.stop()
-    motor_fr.forward()
-    sleep(1)
-    motor_fr.stop()
-    
-test_motors()
-
+while True:
+    char = input("Type a char: ")
+    test_motor(char)
+ 
